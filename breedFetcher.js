ef94697bb06da89e821a2b0b4a6b3651187a5f83
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-02-27 23:29:13 -0500

    Add fetchBreedDescription function inside breedFetcher.js and call it inside index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,36 @@
 
 const request = require('request');
 
-const breedName = process.argv[2];
-const api = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 
-const result = request(api, (error, response, body) => {
-  if (error) {
-    console.log(error);
+const fetchBreedDescription = function(breedName, callback) {
+  const api = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(api, (error, response, body) => {
+    if (error) {
+      
+      callback(error, null);
+    }
+      
+    const data = JSON.parse(body);
+    const breed = data[0];
+    // console.log(breed);
+  
+    if (breed) {
+      
+      callback(null, breed.description);
+    } else {
+      
+      callback(error, null);
+    }
   }
     
-  const data = JSON.parse(body);
-  const breed = data[0];
-  // console.log(breed);
+  );
+};
 
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log('No results found');
-  }
-}
-  
-);
+// fetchBreedDescription(breedName);
+
+module.exports = { fetchBreedDescription };
 
 
 
